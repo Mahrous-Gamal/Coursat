1551654d4c311c65f56a62f443b75f8f6bc500cb
Mahrous-Gamal <mahrousgamal2001@mail.com>
2024-02-19 00:56:43 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,78 @@
+  let scrollingUp = document.querySelector(".scrolling-up");
+  window.onscroll = function () {
+      if (window.scrollY >= 400) scrollingUp.classList.add("show");
+      else scrollingUp.classList.remove("show");
+  };
+  scrollingUp.onclick = function () {
+      window.scrollTo(0, 0);
+  };
+  // Get elements
+  const navlist = document.querySelector(".navbar-nav");
+  const navLinks = document.querySelectorAll(".navbar-nav li a");
+
+  // Handle nav list element on click
+  navlist.addEventListener("click", (e) => {
+    // Remove active class from every element
+    navLinks.forEach((link) => {
+      link.classList.remove("active");
+    });
+
+    // Add active class to current element
+    if (!e.target.classList.contains("active")) {
+      e.target.classList.add("active");
+    }
+  });
+
+document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
+  image_1.addEventListener('click', () =>{
+      var src = image_1.getAttribute('src');
+      document.querySelector('.big-image-1').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-2').forEach(image_2 =>{
+  image_2.addEventListener('click', () =>{
+      var src = image_2.getAttribute('src');
+      document.querySelector('.big-image-2').src = src;
+  });
+});
+
+document.querySelectorAll('.featured-image-3').forEach(image_3 =>{
+  image_3.addEventListener('click', () =>{
+      var src = image_3.getAttribute('src');
+      document.querySelector('.big-image-3').src = src;
+  });
+});
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    820: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+ 
+  },
+});
+
